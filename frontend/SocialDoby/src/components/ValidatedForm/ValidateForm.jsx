7af7019bcb09b90b23b4043d1b8c117c02c6c044
Sam Machado <sam.developer.23@gmail.com>
2023-08-26 22:58:33 +0200

    correccion ValidateForm

--- frontend/SocialDoby/src/components/ValidatedForm/ValidateForm.jsx
+++ frontend/SocialDoby/src/components/ValidatedForm/ValidateForm.jsx
@@ -6,22 +6,19 @@ const ValidatedForm = () => {
         <div className='register-card'>
             <div className='logo-container'>
                 <Logo />
-             </div>
-             <div className='login-input'>
-            <h2>Validación</h2>
-            <p>
-                Por favor, active la cuenta a través del correo de verificación.
-            </p>
-            <p>
-                Gracias por confiar en nosotros, un saludo de parte del equipo
-                TatooArt.
-            </p>
+            </div>
+            <div className='login-input'>
+                <h2>Validación</h2>
+                <p>
+                    Por favor, active la cuenta a través del correo de
+                    verificación.
+                </p>
             </div>
             <div className='button-container'>
-                        <NavLink className='login-button' to='/login'>
-                            Login
-                        </NavLink>
-                </div>
+                <NavLink className='login-button' to='/login'>
+                    Login
+                </NavLink>
+            </div>
         </div>
     );
 };
